0a9dced0981e00f29241b11cbaa2237697acc37f
WingRider2 <72336626+WingRider2@users.noreply.github.com>
2023-04-13 17:43:18 +0900

    체크용입니다

--- multigame/Assets/Script/jin.cs
+++ multigame/Assets/Script/jin.cs
@@ -7,7 +7,7 @@ public class jin : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Áø");
     }
 
     // Update is called once per frame
