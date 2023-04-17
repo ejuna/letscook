d612bad2520696dc15857fff93938d40db7b32f9
kwon nonna <non1233@naver.com>
2023-04-17 14:00:30 +0900

    클래스 만듬

--- multigame/Assets/Script/OrderManager.cs
+++ multigame/Assets/Script/OrderManager.cs
@@ -2,12 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class kwonkwon : MonoBehaviour
+public class OrderManager : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("±Ç±Ç");
+        
     }
 
     // Update is called once per frame
