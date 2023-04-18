fd8678a068cd91e4e14516ba357162b318c8bfaa
SNGYNGJIN <jsy9508@gmail.com>
2023-04-19 01:21:44 +0900

    재료클래스 스크립트

---
+++ multigame/Assets/IngredientAssets/IngredientContainer.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IngredientContainer : MonoBehaviour
+{
+    public Type ingredientType;
+    public Ingredient[] ingredients;
+    public int[] counts;
+}
---
+++ multigame/Assets/IngredientAssets/IngredientManager.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IngredientManager : MonoBehaviour
+{
+    public List<IngredientContainer> ingredientContainers;
+
+    public void display()
+    {
+
+    }
+
+    public void ingredientIncrease(string name, Type IngredeintType)
+    {
+
+    }
+    public void ingredientDecrease(string name, Type IngredeintType)
+    {
+
+    }
+}
