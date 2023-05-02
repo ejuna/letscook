1d0db142994be4865d7cd3a720db44dde6e943f0
kwon nonna <non1233@naver.com>
2023-05-02 16:53:39 +0900

    OrderManager 틀 잡기
    (1)
    List<Order>로 오늘의 음식을 받는게 아니라
    List<Food> 로 받아야함
    (2) day와 fame 에 따라 기본 제한시간이 있어야함
    (3) public bool checkOrder(Order order)
    에서
    public bool checkOrder(Food food)
    받기
    여기서 food는 사용자가 만든 음식.

--- multigame/Assets/Script/Food.cs
+++ multigame/Assets/Script/Food.cs
@@ -15,4 +15,20 @@ public class Food
     ingredients = ing;
     this.price = price;
   }
+
+  public Food(string name, int tier, string ing, int price)
+  {
+    foodName = name;
+    foodTier = tier;
+    ingredients = new List<Ingredient>();
+    //재료 정보 txt를 불러서 일치하는 이름에 Ingredient를 생성하여 Add하기
+    // xxx 걍 OrderManager에서 재료정보 한번만 불러오면 될 일이다. 여기서 처리하지 말자xxx
+    this.price = price;
+  }
+
+  public List<Ingredient> getIngredients(){
+    return ingredients;
+  }
+
+
 }
--- multigame/Assets/Script/Managers/OrderManager.cs
+++ multigame/Assets/Script/Managers/OrderManager.cs
@@ -2,18 +2,119 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class OrderManager : MonoBehaviour
+public class OrderManager
 {
+  private Queue<Order> orderList;
+  private List<Food> todaysOrder;
+  private int complete;
+  private int defaltTime;
+  private const int MAX_GROUP = 4;
 
-    // Start is called before the first frame update
-    void Start()
+  public string fileName = "load_order"; // 불러올 파일의 이름
+
+
+  public OrderManager(int day,int fame){
+    orderList = new Queue<Order>();
+    todaysOrder = createTodaysOrder(day, fame);
+    defaltTime = getDefaltTime(day, fame);
+    complete = 0;
+  }
+
+///txt 파일 불러오고 리스트에 저장.
+  public List<Food> createTodaysOrder(int day, int fame)
+  {
+    List<Food> list = new List<Food>();
+    TextAsset textAsset = Resources.Load<TextAsset>(fileName);
+    string fileContent = textAsset.text;
+    string[] line = fileContent.Split(new[] { "\r\n", "\r", "\n" }, System.StringSplitOptions.None);
+
+    for(int i=0;i<line.Length;i++){
+      string name = line[i].Split('/')[0];
+      int tier = int.Parse(line[i].Split('/')[1]);
+      string ingredient = line[i].Split('/')[2];
+      int price = int.Parse(line[i].Split('/')[3]);
+
+      
+    }
+    return list;
+  }
+
+
+  public void createOrder(){
+    int rand = Random.Range(0, todaysOrder.Count);
+    Order order = new Order(todaysOrder[rand], defaltTime);
+    orderList.Enqueue(order);
+  }
+
+  public void createOrder(int time)
+  {
+    int rand = Random.Range(0, todaysOrder.Count);
+    Order order = new Order(todaysOrder[rand], time);
+    orderList.Enqueue(order);
+  }
+
+
+  public void createGourmandOrder(){
+    createOrder((int)(defaltTime - defaltTime * 0.3));
+  }
+
+  public void createGroupGuestOrder(){
+    int group_num = Random.Range(2, MAX_GROUP);
+    for (int i=0;i<= group_num;i++){
+      createOrder();
+    }
+  }
+
+  public bool checkOrder(Food food){
+    if (orderList.Count == 0)
     {
-        
+      return false;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    Food order_food = orderList.Dequeue().getFood();
+    List<Ingredient> ingredients = food.getIngredients();
+    List<Ingredient> order_ingredients = order_food.getIngredients();
+    int ingredients_size = ingredients.Count;
+    int order_ingredients_size = order_ingredients.Count;
+
+
+    List<bool> check = new List<bool>();
+    for(int i=0;i<order_ingredients.Count;i++){
+      check.Add(false);
     }
+
+
+    for(int i=0;i< ingredients_size; i++){
+      string ingredient = ingredients[i].name;
+      for(int j=0;j< order_ingredients_size; j++){
+        if(check[j] == false && ingredient.Equals(order_ingredients[j].name)){
+          check[j] = true;
+          break;
+        }
+
+        //일치하는 재료가 없을때
+        if (j == order_ingredients_size - 1) return false;
+      }
+    }
+
+    for(int i = 0; i < order_ingredients_size; i++){
+      if (check[i] == false) return false;
+    }
+    return true;
+  }
+
+
+  public bool deleteOrder(){
+    if(orderList.Count == 0){
+      return false;
+    }
+    orderList.Dequeue();
+    return true;
+  }
+
+
+///팀원들과 함께 상의하여 day와 fame에 따라 기본 제한시간을 정하기
+  private int getDefaltTime(int day,int fame){
+    return 0;
+  }
 }
