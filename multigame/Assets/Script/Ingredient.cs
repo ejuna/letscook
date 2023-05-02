46eb19ab1be65a512954a6985ff11574a1315e15
SNGYNGJIN <jsy9508@gmail.com>
2023-05-02 19:17:17 +0900

    ingredientManager

--- multigame/Assets/Script/Ingredient.cs
+++ multigame/Assets/Script/Ingredient.cs
@@ -7,7 +7,7 @@ using UnityEngine;
 public class Ingredient : MonoBehaviour
 {
     public enum Type { StapleFood, Meat, Vegetable };
-    public new string name;
+    public string ingredientName;
     public int price;
     public Type ingredientType;
 }
