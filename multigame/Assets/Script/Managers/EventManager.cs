d869cd872b1d3abd6deeab8bde985010394bbe93
WingRider2 <72336626+WingRider2@users.noreply.github.com>
2023-05-10 17:45:31 +0900

    Update EventManager.cs

--- multigame/Assets/Script/Managers/EventManager.cs
+++ multigame/Assets/Script/Managers/EventManager.cs
@@ -8,6 +8,8 @@ public class EventManager
     float randtime = -1.0f;
     //랜덤으로 생성된 단체손님 수
     int rnadGroupNum = 0;
+    int maxDate = 0;
+    int maxMoney = 0;
     public void OnUpdate()
     {
 
@@ -15,13 +17,22 @@ public class EventManager
         //오늘 주문횟수가 10이면 미식가 이벤트 발생
         if (Managers.Orders.complete == -1)
         {
-            //주문생성에 필요한 변수등의 식을 생성
+            //주문생성에 필요한 변수등의 식을 생성()
             
         }
         //그시간을 이용하여 랜덤한 시간에 단체손님 발생
         if (Managers.Date.time==randtime) {
-            //단체손님 수를 
+            //단체손님 수를 계산(명성,오늘처리한 주문수, 현재주문수,현재까지 성공한 주문수)
             
+        }
+        //목표일짜 및 목표 금액 달성시 게임 클리어
+        if (Managers.Date.day == maxDate || Managers.Money.money == maxMoney)
+        {
+            gameClear();
+        }
+        if (Managers.Life.getLife() <= 0)
+        {
+            gameOver();
         }
     }
     public void gourmandEvent() 
@@ -35,5 +46,14 @@ public class EventManager
         Managers.Orders.createGroupGuestOrder(rnadGroupNum);
     }
 
+    public void gameOver()
+    {
+        //게임 오버씬으로 전환한다.
+    }
+
+    public void gameClear()
+    {
+        //게임 클리어씬으로 전환한다.
+    }
     public void Clear() { }
 }
