334a2ed75a8ae3cce05c7eb78562b01cd85f3af0
ejuna <119400734+ejuna@users.noreply.github.com>
2023-05-17 01:08:31 +0900

    test1
    test1

--- multigame/Assets/Script/Managers/PhotonManager.cs
+++ multigame/Assets/Script/Managers/PhotonManager.cs
@@ -4,55 +4,112 @@ using UnityEngine;
 using Photon.Pun; 
 using Photon.Realtime;
 using UnityEngine.UI;
+using TMPro;
 
 public class PhotonManager : MonoBehaviourPunCallbacks
 {
+    private string gameVersion = "1"; // 게임 버전
+    private string userId;
 
-    InputField m_InputField;
-    Text m_textConnectLog;
-    Text m_textPlayerList;
+    public TMP_InputField userIdText;
+    public TMP_InputField roomNameText;
+
+    private Dictionary<string, GameObject> roomDict = new Dictionary<string, GameObject>();
+    public GameObject roomPrefab;
+    public Transform scrollContent;
+
+    private void Awake()
+    {
+        //씬 자동싱크 설정
+        PhotonNetwork.AutomaticallySyncScene = true;
+
+        // 게임 버전 설정
+        PhotonNetwork.GameVersion = gameVersion;
+
+        //포톤서버 접속
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
 
     void Start()
     {
+        Debug.Log("포톤 매니저 시작");
+        userId = PlayerPrefs.GetString("User_ID", $"User_{Random.Range(0, 100):00}");
+        userIdText.text = userId;
+        PhotonNetwork.NickName = userId;
         Screen.SetResolution(1920, 1080, false);
-
-        m_InputField = GameObject.Find("Canvas/InputField").GetComponent<InputField>();
-        m_textPlayerList = GameObject.Find("Canvas/TextPlayerList").GetComponent<Text>();
-        m_textConnectLog = GameObject.Find("Canvas/TextConnectLog").GetComponent<Text>();
-
-        m_textConnectLog.text = "접속로그\n";
     }
 
     public override void OnConnectedToMaster()
     {
-        RoomOptions options = new RoomOptions();
-        options.MaxPlayers = 4;
+        Debug.Log("포톤 서버 접속");
 
-        PhotonNetwork.LocalPlayer.NickName = m_InputField.text;
-        PhotonNetwork.JoinOrCreateRoom("Room1", options, null);
-
+        PhotonNetwork.JoinLobby();
     }
+
+    public override void OnJoinedLobby()
+    {
+        Debug.Log("로비 접속");
+    }
+
+    public override void OnCreatedRoom()
+    {
+        Debug.Log("방 생성");
+    }
+
     public override void OnJoinedRoom()
     {
-        updatePlayer();
-        m_textConnectLog.text += m_InputField.text;
-        m_textConnectLog.text += " 님이 방에 참가하였습니다.\n";
-    }
+        Debug.Log("방 접속");
 
-
-    public void Connect()
-    {
-        PhotonNetwork.ConnectUsingSettings();
-    }
-
-    void updatePlayer()
-    {
-        m_textPlayerList.text = "접속자";
-        for (int i = 0; i < PhotonNetwork.PlayerList.Length; i++)
+        /*if (PhotonNetwork.IsMasterClient)
         {
-            m_textPlayerList.text += "\n";
-            m_textPlayerList.text += PhotonNetwork.PlayerList[i].NickName;
+            PhotonNetwork.LoadLevel("Level_1");
+        }*/
+
+        GameObject.Find("View").transform.Find("LobbyView").gameObject.SetActive(false);
+        GameObject.Find("View").transform.Find("RoomView").gameObject.SetActive(true);
+
+
+    }
+
+    public override void OnRoomListUpdate(List<RoomInfo> roomList)
+    {
+        GameObject tempRoom = null;
+        foreach(var room in roomList)
+        {
+            if (room.RemovedFromList == true)
+            {
+                roomDict.TryGetValue(room.Name, out tempRoom);
+                Destroy(tempRoom);
+                roomDict.Remove(room.Name);
+            }
+            else
+            {
+                if (roomDict.ContainsKey(room.Name) == false)
+                {
+                    GameObject _room = Instantiate(roomPrefab, scrollContent);
+                    _room.GetComponent<RoomData>().RoomInfo = room;
+                    roomDict.Add(room.Name, _room);
+                }
+                else
+                {
+                    roomDict.TryGetValue(room.Name, out tempRoom);
+                    tempRoom.GetComponent<RoomData>().RoomInfo = room;
+                }
+            }
         }
+    }
+
+    public void OnMakeRoomClick()
+    {
+        RoomOptions ro = new RoomOptions();  //방 옵션 설정
+        ro.IsOpen = true;
+        ro.IsVisible = true;
+        ro.MaxPlayers = 4;
+
+      
+
+        PhotonNetwork.CreateRoom(roomNameText.text, ro);
     }
 
 }
