cdbf1889adfaa259a5ff8a2f071fd24e0812ebdf
WingRider2 <72336626+WingRider2@users.noreply.github.com>
2023-06-03 14:31:00 +0900

    음식 합성

--- multigame/Assets/Script/Controller/CountertopController.cs
+++ multigame/Assets/Script/Controller/CountertopController.cs
@@ -1,15 +1,19 @@
+using Photon.Pun;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class CountertopController : MonoBehaviour
 {
-    List<GameObject> ingres;
+    List<string> ingres;
     bool isPlayerEnter;
     // Start is called before the first frame update
     void Start()
     {
+        ingres = new List<string>();
         Managers.Input.KeyAction -= OnKeyboard;
         Managers.Input.KeyAction += OnKeyboard;
     }
@@ -21,10 +25,51 @@ public class CountertopController : MonoBehaviour
     }
     void OnKeyboard()
     {
-        //조합하기
-        if (Input.GetKey(KeyCode.V) && isPlayerEnter)
+        int loopNum = 0;
+        if (Input.GetKeyDown(KeyCode.V) && isPlayerEnter)
         {
+
             List<FoodData> tempAllFood = Managers.Orders.allFoods.ToList();
+            List<FoodData> produceFood = new List<FoodData>();
+
+            ingres.ForEach(food =>
+            {
+                produceFood.Clear();
+                for (int i = 0; i< tempAllFood.Count;i++)
+                {
+                    bool isSame=false;
+                    if(tempAllFood[i].Ingredients.Count<= ingres.Count)
+                    {
+                        tempAllFood[i].Ingredients.ForEach(IngredientData =>
+                        {
+                            //Debug.Log(IngredientData.name);
+                            if (IngredientData.name.Equals(food))
+                            {
+                                isSame = true;
+                            }
+                            if (loopNum++ > 10000)
+                                throw new Exception("Infinite Loop");
+                        });
+                    }
+                    if (isSame) produceFood.Add(tempAllFood[i]);
+                    
+                }
+                tempAllFood.Clear();
+                tempAllFood = produceFood.ToList();
+            });
+
+            for(int i = 1 ; i< transform.childCount; i++)
+            {
+                Destroy(transform.GetChild(i).gameObject);
+            }
+
+            if (tempAllFood.Count==1) {
+                Managers.Resource.Instantiate("요리/" + tempAllFood[0].name);
+            }
+            else
+            {
+                Managers.Resource.Instantiate("요리/Clinker");
+            }
             //Managers.Orders 체크래시피 참이면 실행 
             //자식을 불러서 usetfood를 생성
             //userfood라는 클래스가 생성이 돼고 그 클래스들을 넘겨준다.
@@ -54,10 +99,12 @@ public class CountertopController : MonoBehaviour
         }
         if (other.gameObject.tag == "Pickup")
         {
+            ingres.Add(other.gameObject.name);
+
             other.transform.SetParent(transform, false);
             other.transform.localPosition = Vector3.zero;
             other.transform.rotation = new Quaternion(0, 1, 0, 0);
-            ingres.Add(other.gameObject);
+
             setEquip(other.gameObject, true);
         }
     }
