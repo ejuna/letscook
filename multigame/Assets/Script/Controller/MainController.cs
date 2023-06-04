1dce4169843b7d5a233ea0fa1e1faa2317c06e23
ejuna <119400734+ejuna@users.noreply.github.com>
2023-06-05 00:57:10 +0900

    1
    1

--- multigame/Assets/Script/Controller/MainController.cs
+++ multigame/Assets/Script/Controller/MainController.cs
@@ -87,7 +87,7 @@ public class MainController : MonoBehaviourPunCallbacks
                         ioc.isTrigger = true;
                         interactingObject.transform.localPosition = Vector3.zero;
 
-                        pv.RPC("UpdateInteractingObjectPosition", RpcTarget.Others,interactingObject,GameObject);
+                        pv.RPC("UpdateInteractingObjectPosition", RpcTarget.Others);
                         Debug.Log("부모 설정 실행시켜");
 
                         isPicking = true; // 들고 있는지 아닌지 체크
@@ -200,11 +200,11 @@ public class MainController : MonoBehaviourPunCallbacks
     }
 
     [PunRPC]
-    private void UpdateInteractingObjectPosition(GameObject obj,GameObject player)
+    private void UpdateInteractingObjectPosition()
     {
         
-        obj.transform.SetParent(player.transform);
+        
 
-        Debug.Log("부모 받아서 설정");
+        Debug.Log("실행했음");
     }
 }
