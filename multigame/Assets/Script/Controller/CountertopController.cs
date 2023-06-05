5f6c571dba1046c5cdf9ed022d473fc4d51d80a0
WingRider2 <72336626+WingRider2@users.noreply.github.com>
2023-06-05 22:07:10 +0900

    Update CountertopController.cs

--- multigame/Assets/Script/Controller/CountertopController.cs
+++ multigame/Assets/Script/Controller/CountertopController.cs
@@ -16,6 +16,8 @@ public class CountertopController : MonoBehaviourPun, IPunObservable
     // Start is called before the first frame update
     void Start()
     {
+        PV = photonView;
+        if (PV.IsMine) PV.RPC(nameof(makeFood), RpcTarget.All);
         ingres = new List<string>();
         //st = "";
         Managers.Input.KeyAction -= OnKeyboard;
@@ -29,89 +31,95 @@ public class CountertopController : MonoBehaviourPun, IPunObservable
     }
     void OnKeyboard()
     {
-        int loopNum = 0;
+        
         if (Input.GetKeyDown(KeyCode.V) && isPlayerEnter)
         {
-            /*
-            if (st.Length != 0)
-            {
-                String[] starr = st.Split(",");
-                for (int i = 0; i < starr.Length-1; i++)
-                {
-                    Debug.Log(i);
-                    ingres.Add(starr[i]);
-                }
-            }
-            */
-            if (ingres.Count == 0)
-            {
-                return;
-            }
-            
-            List<FoodData> tempAllFood = Managers.Orders.allFoods.ToList();
-            List<FoodData> produceFood = new List<FoodData>();
-            //해당 재료로 만들수 있는 요리찾기
-            //들어온 재료와 음식중 해당 재료를 가진 음식 찾기
-            //들어온 재료 반복문
-            ingres.ForEach(food =>
-            {
-                produceFood.Clear();
-                //기존요리들 전부 검색
-                for (int i = 0; i< tempAllFood.Count;i++)
-                {
-                    bool isSame=false;//무한루프 방지
-                    //들어온 재료 보다 재료많은 요리 거르기
-                    if(tempAllFood[i].Ingredients.Count<= ingres.Count)
-                    {
-                        //기존음식의 재료중 같은 거 있는지 찾기
-                        tempAllFood[i].Ingredients.ForEach(IngredientData =>
-                        {
-                            //Debug.Log(IngredientData.name);
-                            if (IngredientData.name.Equals(food))
-                            {
-                                isSame = true;
-                            }
-                            if (loopNum++ > 10000) throw new Exception("Infinite Loop"); //무한루프방지
-                        });
-                    }
-                    if (isSame) produceFood.Add(tempAllFood[i]);//같은거를 모은 리스트에 추가
-                    
-                }
-
-                tempAllFood.Clear();
-                tempAllFood = produceFood.ToList();
-            });
-
-            //st = "";
-            ingres.Clear();
-            ingres = new List<string>();
-            //재료 삭제하기
-            for (int i = 1 ; i< transform.childCount; i++)
-            {
-                Debug.Log(transform.GetChild(i).gameObject.name);
-                PhotonNetwork.Destroy(transform.GetChild(i).gameObject);
-            }
-            //음식 생성
-            GameObject go;
-            Vector3 newPosition = transform.position + new Vector3(2f, 2f, 0f);
-            
-            if (tempAllFood.Count==1) {
-                Debug.Log("완성");
-                go = PhotonNetwork.Instantiate("Prefabs/요리/" + tempAllFood[0].name , newPosition,transform.rotation);
-            }
-            else//실패 음식
-            {
-                go= PhotonNetwork.Instantiate("Prefabs/요리/Clinker", newPosition, transform.rotation);
-            }
-            go.tag = "Pickup";
-            go.AddComponent<Rigidbody>();
-            go.GetComponent<Rigidbody>().freezeRotation = true;
-            go.AddComponent<BoxCollider>();
-            go.GetComponent<BoxCollider>().size = new Vector3(1f, 1.2f, 1f);
-            go.GetComponent<BoxCollider>().center = new Vector3(0, go.GetComponent<BoxCollider>().size.y/2,0);
+            makeFood();
         }
     }
+    [PunRPC]
+    void makeFood()
+    {
+        /*
+if (st.Length != 0)
+{
+    String[] starr = st.Split(",");
+    for (int i = 0; i < starr.Length-1; i++)
+    {
+        Debug.Log(i);
+        ingres.Add(starr[i]);
+    }
+}
+*/
+        if (ingres.Count == 0)
+        {
+            return;
+        }
 
+        List<FoodData> tempAllFood = Managers.Orders.allFoods.ToList();
+        List<FoodData> produceFood = new List<FoodData>();
+        //해당 재료로 만들수 있는 요리찾기
+        //들어온 재료와 음식중 해당 재료를 가진 음식 찾기
+        //들어온 재료 반복문
+        int loopNum = 0;
+        ingres.ForEach(food =>
+        {
+            produceFood.Clear();
+            //기존요리들 전부 검색
+            for (int i = 0; i < tempAllFood.Count; i++)
+            {
+                bool isSame = false;//무한루프 방지
+                                    //들어온 재료 보다 재료많은 요리 거르기
+                if (tempAllFood[i].Ingredients.Count <= ingres.Count)
+                {
+                    //기존음식의 재료중 같은 거 있는지 찾기
+                    tempAllFood[i].Ingredients.ForEach(IngredientData =>
+                    {
+                        //Debug.Log(IngredientData.name);
+                        if (IngredientData.name.Equals(food))
+                        {
+                            isSame = true;
+                        }
+                        if (loopNum++ > 10000) throw new Exception("Infinite Loop"); //무한루프방지
+                    });
+                }
+                if (isSame) produceFood.Add(tempAllFood[i]);//같은거를 모은 리스트에 추가
+
+            }
+
+            tempAllFood.Clear();
+            tempAllFood = produceFood.ToList();
+        });
+
+        //st = "";
+        ingres.Clear();
+        ingres = new List<string>();
+        //재료 삭제하기
+        for (int i = 1; i < transform.childCount; i++)
+        {
+            Debug.Log(transform.GetChild(i).gameObject.name);
+            PhotonNetwork.Destroy(transform.GetChild(i).gameObject);
+        }
+        //음식 생성
+        GameObject go;
+        Vector3 newPosition = transform.position + new Vector3(2f, 2f, 0f);
+
+        if (tempAllFood.Count == 1)
+        {
+            Debug.Log("완성");
+            go = PhotonNetwork.Instantiate("Prefabs/요리/" + tempAllFood[0].name, newPosition, transform.rotation);
+        }
+        else//실패 음식
+        {
+            go = PhotonNetwork.Instantiate("Prefabs/요리/Clinker", newPosition, transform.rotation);
+        }
+        go.tag = "Pickup";
+        go.AddComponent<Rigidbody>();
+        go.GetComponent<Rigidbody>().freezeRotation = true;
+        go.AddComponent<BoxCollider>();
+        go.GetComponent<BoxCollider>().size = new Vector3(1f, 1.2f, 1f);
+        go.GetComponent<BoxCollider>().center = new Vector3(0, go.GetComponent<BoxCollider>().size.y / 2, 0);
+    }
     public void setEquip(GameObject gameObject, bool isEquip)
     {
         Collider[] gameObjectColliders = gameObject.GetComponents<Collider>();
