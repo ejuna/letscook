21c25ad016e8e577b2a0ad42fc07d86ac8c204e9
ejuna <119400734+ejuna@users.noreply.github.com>
2023-06-06 01:51:24 +0900

    게임씬 이동시 inLobbyRoom false 설정
    게임씬 이동시 inLobbyRoom false 설정

--- multigame/Assets/Script/Managers/PhotonManager.cs
+++ multigame/Assets/Script/Managers/PhotonManager.cs
@@ -383,6 +383,7 @@ public class PhotonManager : MonoBehaviourPunCallbacks
         {
             
             PhotonNetwork.LoadLevel("Game");
+            inLobbyRoom = false;
 
         }
     }
